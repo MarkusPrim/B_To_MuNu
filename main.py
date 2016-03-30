4e44e3e25f70b461e0449d31d1579f7e52bc214f
Markus Prim <ufcqr@student.kit.edu>
2016-03-30 20:22:39 +0200

    Calculate with uncertainties

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy
+from uncertainties import ufloat
 
 
 class Unit(object):
@@ -27,27 +28,26 @@ class BToMuNuCalculations(object):
 
         :return:
         """
-        self.fermi_coupling_constant = 1.1663786e-5 * Unit.GeV**-2
-        self.b_meson_mass = 5279.26 * Unit.MeV
-        self.b_meson_lifetime = 1.638e-12 * Unit.s
-        self.decay_constant = 0.191 * Unit.GeV
-        self.v_ub = 4.13e-3
-        self.electron_mass = 0.510998929 * Unit.MeV
-        self.muon_mass = 105.6583715 * Unit.MeV
-        self.tau_mass = 1776.82 * Unit.MeV
+        self.fermi_coupling_constant = ufloat(1.1663786e-5 * Unit.GeV**-2, 0 * Unit.GeV**-2, tag="G_F")
+        self.b_meson_mass = ufloat(5279.26 * Unit.MeV, 0.17 * Unit.MeV, tag="M_B")
+        self.b_meson_lifetime = ufloat(1.638e-12 * Unit.s, 0.004e-12 * Unit.s, tag="tau_B")
+        self.decay_constant = ufloat(0.191 * Unit.GeV, 0.009 * Unit.GeV, tag="f_B")
+        # self.v_ub = ufloat(4.13e-3, 0.49e-3, tag="V_ub")  # PDG
+        self.v_ub = ufloat(3.95e-3, numpy.sqrt(0.39e-3**2 + 0.38e-3**2), tag="V_ub")
+        self.electron_mass = ufloat(0.510998929 * Unit.MeV, 0.000000022 * Unit.MeV, tag="m_e")
+        self.muon_mass = ufloat(105.6583715 * Unit.MeV, 0.0000035 * Unit.MeV, tag="m_mu")
+        self.tau_mass = ufloat(1776.82 * Unit.MeV, 0.16 * Unit.MeV, tag="m_tau")
 
     def standard_model_branching_ratio(self, lepton_mass):
         """ Standard Model prediction for the given lepton mass.
         Equation taken from Physics at the B-factories p.396.
 
         :param lepton_mass:
-        :return:
+        :return: branching ratio
         """
-        x = self.fermi_coupling_constant**2 * self.b_meson_mass * lepton_mass**2 / (8 * numpy.pi)
-        helicity_suppression = (1 - (lepton_mass / self.b_meson_mass)**2)**2
-        y = self.decay_constant**2 * self.v_ub**2 * self.b_meson_lifetime
-        return x * helicity_suppression * y
-
+        return self.fermi_coupling_constant**2 * self.b_meson_mass * lepton_mass**2 / (8 * numpy.pi) \
+            * (1 - (lepton_mass / self.b_meson_mass)**2)**2 \
+            * self.decay_constant**2 * self.v_ub**2 * self.b_meson_lifetime
 
 if __name__ == '__main__':
     calculator = BToMuNuCalculations()
@@ -55,9 +55,11 @@ if __name__ == '__main__':
     mu = calculator.standard_model_branching_ratio(calculator.muon_mass)
     tau = calculator.standard_model_branching_ratio(calculator.tau_mass)
 
+    print("Standard Model branching ratio predictions:")
     print("BR(B->e nu):\t{:.2e}".format(e))
     print("BR(B->mu nu):\t{:.2e}".format(mu))
     print("BR(B->tau nu):\t{:.2e}".format(tau))
-
+    print()
+    print("Electron and Muon helicity suppression compared to Tau Channel:")
     print("BR(B->e nu) / BR(B->tau nu):\t{:.2e}".format(e/tau))
     print("BR(B->mu nu) / BR(B->tau nu):\t{:.2e}".format(mu/tau))
